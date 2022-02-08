2f36ccce53c7c6abe103b8ffccab7590b6ff5300
Ben Martinez <56284093+benmrtnz27@users.noreply.github.com>
2022-02-07 20:23:13 -0800

    Update triangles.asm

--- triangles.asm
+++ triangles.asm
@@ -26,10 +26,13 @@ good_bye: db "Have a good day", 10, 0
 ; displays
 good_morning: db "Good morning %s. ", 0
 output_three_float: db "Thank you. You entered %7.5lf %7.5lf %7.5lf", 10, 0
+results: db "The area of your triangle is %lf square units", 10, "the perimeter is %lf linear units.", 10
 
 segment .bss
 
 user_name: resb 64
+perimeter: resq 1
+area: resq 1
 
 segment .text
 triangles:
@@ -116,13 +119,70 @@ triangles:
 
 	;--- CONVERT XMM13 INTO RADIANS ---
 	movsd xmm12, [pi]	; set xmm12 to pi
-	mov rax, 180.0
+	mov rax, 180
 	cvtsi2sd xmm11, rax	; xmm11 = 180
 	mulsd xmm13, xmm12	; rad * deg
 	divsd xmm13, xmm11	; (rad * deg) / 180.0
 
 	;--- CALCULATE PERIMETER ---
-	
+	; first we need to get length of side 3. We only have side 1, side 2, and the angle between them
+	; Formula for side 3: sqrt(A^2 + B^2 - 2AB*cos(x)) A = side1(xmm15) B = side2(xmm14)
+
+	; 2AB
+	mov rax, 2
+	cvtsi2sd xmm12, rax	;xmm12 = 2
+	mulsd xmm12, xmm15	;2*A
+	mulsd xmm12, xmm14	;(2*A)*B
+
+
+	;A^2
+	movsd xmm11, xmm15	;copy value of xmm15 (A) to xmm11
+	mulsd xmm11, xmm11	;multiple xmm11 by itself A^2
+
+	;B^2
+	movsd xmm10, xmm14
+	mulsd xmm10, xmm10
+
+	; cos(x)
+	movsd xmm0, xmm13
+	call cos ; xmm0 = cos(x)
+
+	mov rax, -1
+	cvtsi2sd xmm9, rax
+	mulsd xmm9, xmm12	; -1 * 2AB = -2AB
+	mulsd xmm9, xmm0	; xmm9 = -2AB*cos(x)
+	addsd xmm9, xmm10	; xmm9 = B^2 - 2AB*cos(x)
+	addsd xmm9, xmm11	; xmm9 = A^2 + B^2 - 2AB*cos(x)
+	sqrtsd xmm9, xmm9	; xmm9 = sqrt(xmm9)
+
+	; add all 3 sides. xmm15 = side 1, xmm14 = side 2, xmm9 = side 3
+	addsd xmm9, xmm15	; xmm9 = xmm9 + xmm15
+	addsd xmm9, xmm14	; xmm9 = xmm9 + xmm14
+
+	movsd [perimeter], xmm9
+
+	; --- CALCULATE AREA ---
+	; Formula for area:1/2AB*sin(angle)
+
+	movsd xmm0, xmm13
+	call sin	; xmm0 = angle. xmm0 = sin(angle)
+
+	;perform formula
+	movsd xmm12, xmm0	; copy sin(angle) into xmm12
+	mulsd xmm12, xmm15	; A*sin(angle)
+	mulsd xmm12, xmm14	; A*B*sin(angle)
+	mov rax, 2
+	cvtsi2sd xmm11, rax
+	divsd xmm12, xmm11	; (A*B*sin(angle))/2
+	movsd [area], xmm12	; move area into [area]
+
+
+	; --- PRINT RESULTS ---
+	movsd xmm0, [perimeter]
+	movsd xmm1, [area]
+	mov rdi, results
+	mov rax, 2
+	call printf
 
 	pop rax
 
